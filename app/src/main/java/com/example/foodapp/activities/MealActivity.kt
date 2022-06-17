e5c58d883bcc49bf2aca83f95de39bed19cec4b4
Sarah Chalup <a2018112520@estudiantes.upsa.edu.bo>
2022-06-17 14:48:56 -0400

    categoria  y area con informacion de la comida

--- app/src/main/java/com/example/foodapp/activities/MealActivity.kt
+++ app/src/main/java/com/example/foodapp/activities/MealActivity.kt
@@ -3,24 +3,43 @@ package com.example.foodapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.foodapp.R
 import com.example.foodapp.databinding.ActivityMealBinding
 import com.example.foodapp.fragments.HomeFragment
+import com.example.foodapp.pojo.Meal
+import com.example.foodapp.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMealBinding
     private lateinit var mealId: String
     private lateinit var mealName: String
     private lateinit var mealThumb: String
+    private lateinit var mealMvvm: MealViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mealMvvm = ViewModelProvider(this)[MealViewModel::class.java]
 
         getMealInformationFromIntent()
         setInformationIView()
+        mealMvvm.getMealDetail(mealId)
+        obseveMealDetailsLifeData()
+    }
+
+    private fun obseveMealDetailsLifeData() {
+        mealMvvm.observeMealsDetailsLiveData().observe(this, object  : Observer<Meal>{
+            override fun onChanged(t: Meal?) {
+                val meal = t
+                binding.tvCategories.text = ("Categoría: ${meal!!.strCategory}")
+                binding.tvArea.text = ("Area: ${meal!!.strArea}")
+                binding.tvInstructions.text = meal!!.strInstructions
+            }
+        })
     }
 
     private fun setInformationIView() {
--- app/src/main/java/com/example/foodapp/viewModel/MealViewModel.kt
+++ app/src/main/java/com/example/foodapp/viewModel/MealViewModel.kt
@@ -13,21 +13,22 @@ import retrofit2.Response
 
 class MealViewModel:ViewModel() {
 private var mealDetailsLiveData = MutableLiveData<Meal>()
-/*
+
     fun getMealDetail(id:String)
     {
-        RetrofitInstance.api.getMealDetails(id).enqueue(object  : Callback<MealList>{
+        RetrofitInstance.api.getMealDeails(id).enqueue(object  : Callback<MealList>{
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
                 if (response.body()!=null){
                     mealDetailsLiveData.value = response.body()!!.meals[0]
-                }
+                }else
+                    return
             }
 
             override fun onFailure(call: Call<MealList>, t: Throwable) {
                 Log.d("MealActivity", t.message.toString())
             }
         })
-    }*/
+    }
     fun observeMealsDetailsLiveData():LiveData<Meal>{
         return mealDetailsLiveData
     }
